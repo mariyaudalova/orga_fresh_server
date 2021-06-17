e66aefd8ef431bdde26e208167899353a2324b52
Leonid Semeniuk <leo.sam@ukr.net>
2021-06-17 23:35:19 +0300

    fixed uri

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,5 +1,5 @@
 module.exports = {
   mongoURI:
-    'mongodb+srv://fe20-leonid-semeniuk:YjeZvSJdPC9rT6xV@cluster0.yzjth.mongodb.net/fe20-semeniuk?retryWrites=true&w=majority',
+    'mongodb+srv://fe-20_final_project:JdI0UsAjm2jMMF8v@cluster0.4q3uo.mongodb.net/fe-20_final_project?retryWrites=true&w=majority',
   secretOrKey: 'rad ndoma str0d0 1w21anm1',
 };
