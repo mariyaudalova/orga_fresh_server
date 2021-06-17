cfdc4b56f166d0ef83c7403389f42ebc90bff55e
Mariia Udalova <ASCHA222011@gmail.com>
2021-06-17 23:05:41 +0300

    fix

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,5 +1,5 @@
 module.exports = {
   mongoURI:
-    "mongodb+srv://orga_fresh_db:w5oG5JHpP2aPFD84@cluster0.23ml8.mongodb.net/orga_fresh_db?retryWrites=true&w=majority",
+    "mongodb+srv://orga_fresh_db:X9R5eZWrxLtQRL8@cluster0.23ml8.mongodb.net/orga_fresh_db?retryWrites=true&w=majority",
   secretOrKey: "rad ndoma str0d0 1w21anm1",
 };
